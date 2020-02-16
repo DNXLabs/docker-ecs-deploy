cd78031679e726984ad22b58ae18608879cbbd19
Helder Klemp <helderklemp@gmail.com>
2020-02-16 21:23:12 +1100

    improving logs

--- src/tail-task-logs.py
+++ src/tail-task-logs.py
@@ -8,29 +8,27 @@ cluster_name=os.environ['CLUSTER_NAME']
 app_name=os.environ['APP_NAME']
 task_arn=os.environ['TASK_ID']
 app_command=os.environ['DEFAULT_COMMAND']
-
+task_number=task_arn.split(":task/",1)[1]  #get the task number id
 last_event = None
 
 while True:
   try:
     response = aws_ecs.describe_tasks(
       cluster=cluster_name,
-      tasks=[
-        task_arn
-      ]
-    )
+      tasks=[task_arn])
     logs = boto3.client('logs')
     task_status = response['tasks'][0]['lastStatus']
     events_collected = []
     print('Task status', task_status)
     logGroupName='/ecs/'+cluster_name+'/'+app_name
     print('Searching logs for ', logGroupName)
+    time.sleep(4)
     logStreams = logs.describe_log_streams(
         logGroupName=logGroupName,
-        logStreamNamePrefix=app_name+'/'+app_command,
+        logStreamNamePrefix=app_name+'/'+app_command+'/'+app_name+'/'+task_number,
         limit=1,
-        descending=True,)
-    for stream in logStreams['logStreams']:
+        descending=True)
+    for stream in logStreams['logStreams']:      
       streamName=stream['logStreamName']
       print('log Streams', streamName)  
       logStreamEvents = logs.get_log_events(
