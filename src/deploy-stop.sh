03bf9ef8907f8b31d60bab1cc12cb16e266eb3c9
Allan Denot <adenot@gmail.com>
2020-04-08 21:05:32 +1000

    Fixing missing task_set_id error

--- src/deploy-stop.sh
+++ src/deploy-stop.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
+ERROR=0
 if [[ -z "$AWS_DEFAULT_REGION" ]]; then echo "---> ERROR: Missing variable AWS_DEFAULT_REGION"; ERROR=1; fi
 if [[ -z "$APP_NAME" ]];           then echo "---> ERROR: Missing variable APP_NAME"; ERROR=1; fi
 if [[ -z "$CLUSTER_NAME" ]];       then echo "---> ERROR: Missing variable CLUSTER_NAME"; ERROR=1; fi
+if [[ "$ERROR" == "1" ]]; then exit 1; fi
 
 # Fetch deployment ID pending cutover to the green(new) enviroment
 DEPLOYMENT_ID=$(aws deploy list-deployments --application-name=$CLUSTER_NAME-$APP_NAME --deployment-group=$CLUSTER_NAME-$APP_NAME --max-items=1 --query="deployments[0]" --output=text | head -n 1)
--- src/deploy.sh
+++ src/deploy.sh
@@ -20,11 +20,9 @@ cat task-definition.json
 export TASK_ARN=$(aws ecs register-task-definition --cli-input-json file://./task-definition.json | jq --raw-output '.taskDefinition.taskDefinitionArn')
 
 envsubst < app-spec.tpl.json > app-spec.json
-echo
 echo "---> App-spec for CodeDeploy"
 cat app-spec.json
 
-echo
 echo "---> Creating deployment with CodeDeploy"
 
 set +e # disable bash exit on error
@@ -82,10 +80,7 @@ WAIT_PERIOD=0
 MAX_WAIT=300 #$(aws ecs describe-services --cluster $CLUSTER_NAME --service $APP_NAME --query services[0].healthCheckGracePeriodSeconds --output text)
 MAX_WAIT_BUFFER=60
 
-echo
-echo
 echo "---> Waiting $((MAX_WAIT + MAX_WAIT_BUFFER)) seconds for tasks to stabilise"
-echo
 
 while [ "$(aws deploy get-deployment --deployment-id $DEPLOYMENT_ID --query deploymentInfo.status --output text)" == "Ready" ]
 do
@@ -97,16 +92,13 @@ do
 done
 
 DEPLOYMENT_STATUS=$(aws deploy get-deployment --deployment-id $DEPLOYMENT_ID --query deploymentInfo.status --output text)
-echo
 echo "---> Deployment status: $DEPLOYMENT_STATUS"
-echo
 
-if [ "$DEPLOYMENT_STATUS" == "Failed" ]
+if [ "$DEPLOYMENT_STATUS" == "Failed" ] && [ "$TASK_SET_ID" != "" ]
 then
   TASK_ARN=$(aws ecs list-tasks --cluster dev --desired-status STOPPED --started-by $TASK_SET_ID --query taskArns[0] --output text)
   if [ "${TASK_ARN}" != "None" ]; then
     echo "---> Displaying logs of STOPPED task: $TASK_ARN"
-    echo
     /work/tail-task-logs.py $TASK_ARN
   fi
   RET=1
@@ -119,10 +111,8 @@ then
 fi
 
 if [ $RET -eq 0 ]; then
-  echo
   echo "---> Completed!"
 else
-  echo
   echo "---> ERROR: Deployment FAILED!"
 fi
 
