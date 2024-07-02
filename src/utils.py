c0d08d922a497e02ad2ef79976bf0f927a6d1032
lucasmacedot <lucas.macedo@dnx.solutions>
2024-07-01 22:34:07 -0400

    Remove unused comments

--- src/utils.py
+++ src/utils.py
@@ -43,6 +43,3 @@ def json_template(json_template: str, env_vars=os.environ):
         print(f"Error validating JSON. Error: '{err}'")
 
     return template
-
-
-#   "command": [ "sh", "-c", "export DD_AGENT_HOST=$(curl http://169.254.169.254/latest/meta-data/local-ipv4); ${COMMAND}" ],
